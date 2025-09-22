902314d1a5f392e19972b773b841a0aac3832d6d
NetAcad DEVASC <changeme@example.com>
2025-09-22 09:43:05 +0000

    Revised the part of 0-based indexing for user comfortability

--- todo.py
+++ todo.py
@@ -3,26 +3,25 @@ tasks = []
 
 def add_task(task):
     tasks.append(task)
-    print("Task added!")
-    print("\n")
+    print("Task added!\n")
 
-# Causes confusion in indexing
+
 def show_tasks():
     if len(tasks) == 0:
-        print("No tasks yet")
+        print("No tasks yet\n")
     else:
         print("List of Tasks")
         for i in range(len(tasks)):
             print(i + 1, ".", tasks[i])
-
-    print ("\n")
+        print("\n")
 
 
 def remove_task(task_number):
-    tasks.pop(task_number)
-    print("Task removed!!")
-
-
+    if 1 <= task_number <= len(tasks):
+        tasks.pop(task_number - 1)  # Subtract to match list index
+        print("Task removed!!\n")
+    else:
+        print("Invalid task number!\n")
 
 
 def main():
@@ -30,8 +29,8 @@ def main():
         print("1. Add Task")
         print("2. Show Tasks")
         print("3. Remove Task")
-        print("4. Exit")
-        print("\n")
+        print("4. Exit\n")
+        
         choice = input("Enter choice: ")
         if choice == "1":
             task = input("Enter task: ")
@@ -39,12 +38,15 @@ def main():
         elif choice == "2":
             show_tasks()
         elif choice == "3":
-            index = int(input("Enter task number to remove: "))
-            remove_task(index)
+            if len(tasks) == 0:
+                print("No tasks to remove!\n")
+            else:
+                index = int(input("Enter task number to remove: "))
+                remove_task(index)
         elif choice == "4":
             break
         else:
-            print("Wrong choice!!")
+            print("Wrong choice!!\n")
 
 
 if __name__ == "__main__":
